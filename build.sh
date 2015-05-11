a9a860f3a040139b8187bb4cca9eb87aa3a5e12b
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-11 13:08:37 +0800

    add shell script to generate amd

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-browserify src/svgcanvas.js -s SVGCanvas > dist/svgcanvas.js
+browserify src/svgcanvas.js -s SVGCanvas > dist/umd/svgcanvas.js
+
+c2s=$(cat src/canvas2svg.js | sed 's/module.exports = ctx/C2S = ctx/' | sed 's/^/    /' | sed 's/^ *$//')
+svgcanvas=$(cat src/svgcanvas.js | sed 's/var C2S = require.*//' | sed 's/module.exports.*//' | sed 's/^/    /' | sed 's/^ *$//')
+cat > dist/amd/svgcanvas.js <<EOF
+define(function() {
+    var C2S;
+
+$c2s
+$svgcanvas
+    return SVGCanvas;
+});
+EOF
