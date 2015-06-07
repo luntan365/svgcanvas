a345a69651169622f3c2512592cab4617f09ec08
Zeno Zeng <zenoofzeng@gmail.com>
2015-06-07 18:58:38 +0800

    add more tests for arcTo

--- test/context.js
+++ test/context.js
@@ -94,11 +94,52 @@ describe('Context API', function() {
             ctx.stroke();
         });
 
+        testRender('arcTo: If the point (x0, y0) is equal to the point (x1, y1), then the method must add the point (x1, y1) to the subpath, and connect that point to the previous point (x0, y0) by a straight line.', function(ctx) {
+            ctx.beginPath();
+            ctx.moveTo(0, 10);
+            ctx.arcTo(0, 10, 100, 100, 10);
+            ctx.lineTo(50, 50);
+            ctx.lineWidth = 3;
+            ctx.strokeStyle = 'black';
+            ctx.stroke();
+        });
+
+        testRender('arcTo: If the point (x1, y1) is equal to the point (x2, y2), then the method must add the point (x1, y1) to the subpath, and connect that point to the previous point (x0, y0) by a straight line.', function(ctx) {
+            ctx.beginPath();
+            ctx.moveTo(0, 10);
+            ctx.arcTo(100, 100, 100, 100, 10);
+            ctx.lineTo(50, 50);
+            ctx.lineWidth = 3;
+            ctx.strokeStyle = 'black';
+            ctx.stroke();
+        });
+
+        testRender('arcTo: If the radius radius is zero, then the method must add the point (x1, y1) to the subpath, and connect that point to the previous point (x0, y0) by a straight line.', function(ctx) {
+            ctx.beginPath();
+            ctx.moveTo(0, 10);
+            ctx.arcTo(50, 100, 100, 0, 0);
+            ctx.lineTo(50, 50);
+            ctx.lineWidth = 3;
+            ctx.strokeStyle = 'black';
+            ctx.stroke();
+        });
+
+        testRender('arcTo: if the points (x0, y0), (x1, y1), and (x2, y2) all lie on a single straight line, then the method must add the point (x1, y1) to the subpath, and connect that point to the previous point (x0, y0) by a straight line', function(ctx) {
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.arcTo(50, 50, 100, 100, 10);
+            ctx.lineTo(100, 0);
+            ctx.lineWidth = 3;
+            ctx.strokeStyle = 'black';
+            ctx.stroke();
+        });
+
         testRender('arcTo: must connect the point (x0, y0) to the start tangent point by a straight line', function(ctx) {
             ctx.beginPath();
             ctx.lineWidth = 10;
             ctx.moveTo(0, 0);
             ctx.arcTo(100, 100, 0, 100, 20);
+            ctx.lineWidth = 3;
             ctx.strokeStyle = '#000';
             ctx.stroke();
         });
